21403cb41411972a145944975579d79ac7233a38
aniruddhapai <agpai2@illinois.edu>
2020-04-19 16:53:43 -0500

    Implemented the functions that were declared in engine.h
    - The implemented functions are in engine.cc

--- src/engine.cc
+++ src/engine.cc
@@ -8,5 +8,49 @@
 
 namespace mylibrary {
 
+// Converts a direction into a delta location.
+Location FromDirection(const Direction direction) {
+  switch (direction) {
+    case Direction::kUp:
+      return {-1, 0};
+    case Direction::kDown:
+      return {+1, 0};
+    case Direction::kLeft:
+      return {0, -1};
+    case Direction::kRight:
+      return {0, +1};
+  }
+}
+
+Player Engine::GetPlayer() const { return player_; }
+
+void Engine::Reset() {
+  Location location(2 * kLocConstant, kLocConstant);
+  player_.SetLoc(location);
+}
+
+Engine::Engine(int width, int height)
+    : width_{width},
+      height_{height},
+      direction_{Direction::kRight},
+      last_direction_{Direction::kUp},
+      player_{Location(0,0)} {
+  Reset();
+}
+
+void Engine::SetDirection(const mylibrary::Direction direction) {
+  direction_ = direction;
+}
+
+void Engine::Step() {
+  Location initial_loc = FromDirection(direction_);
+  Location new_loc =
+      (player_.GetLoc() + initial_loc) % Location(height_, width_);
+
+  Location leader = new_loc;
+  player_.SetLoc(leader);
+
+  last_direction_ = direction_;
+}
 
 }
