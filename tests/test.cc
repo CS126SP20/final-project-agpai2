47fa9fc49e0c26049e4671edbc2668028f16d61b
aniruddhapai <agpai2@illinois.edu>
2020-04-26 03:37:36 -0500

    Wrote some test cases to check for Locations
    - Tested all operators on location and also checked if
      valid row and columns were returned

--- tests/test.cc
+++ tests/test.cc
@@ -2,14 +2,39 @@
 
 #define CATCH_CONFIG_MAIN
 
-#include <catch2/catch.hpp>
 #include <cinder/Rand.h>
 
-#include <zelda/example.h>
+#include <catch2/catch.hpp>
 
-#include <zelda/location.h>
+#include "../include/zelda/location.h"
+#include "../include/zelda/engine.h"
+#include "../include/zelda/direction.h"
+#include "../include/zelda/player.h"
+#include "../include/zelda/monster.h"
+#include "../include/zelda/map.h"
 
 using zelda::Location;
+using zelda::Engine;
+using zelda::Direction;
+using zelda::Player;
+using zelda::Monster;
+using zelda::Map;
+
+std::vector<std::vector<char>> sample_screen = {{ '1','1','1','0','0','1','0','0','1','0','0','0','1','0','1','0','0','1','0','1','1'},
+                                                { '1','1','1','0','0','1','0','0','1','0','0','0','1','0','1','0','0','1','0','1','1'},
+                                                { '1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'},
+                                                { '1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'},
+                                                { 'h','0','0','0','0','1','0','0','0','0','0','0','1','0','0','0','0','1','0','1','1'},
+                                                { 'h','0','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','j'},
+                                                { 'h','0','0','0','0','1','0','1','1','0','0','1','1','0','1','0','0','1','0','0','j'},
+                                                { 'h','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','j'},
+                                                { 'h','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','j'},
+                                                { '1','0','0','0','0','1','0','0','0','0','0','0','1','0','0','0','0','1','0','1','1'},
+                                                { '1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'},
+                                                { '1','1','1','1','0','1','0','1','1','0','0','1','1','0','1','0','0','1','0','1','1'},
+                                                { '1','1','1','1','e','1','e','e','1','e','e','e','1','e','1','e','e','1','e','1','1' }};
+
+Map sample_map = Map(sample_screen);
 
 TEST_CASE("Location vector operations", "[location]") {
   SECTION("Modulo positive") {
@@ -26,5 +51,27 @@ TEST_CASE("Location vector operations", "[location]") {
 
     Location result = loc1 % loc2;
     REQUIRE(result == Location{6, 1});
+  }
+
+  SECTION("Add Locations") {
+    Location loc1{4, 3};
+    Location loc2{2, 5};
+
+    Location result = loc1 + loc2;
+    REQUIRE(result == Location{6, 8});
+  }
+}
+
+TEST_CASE("Location Values", "[location]") {
+  Location loc{6, 10};
+
+  SECTION("Row of Location") {
+    int row_result = loc.Row();
+    REQUIRE(row_result == 6);
+  }
+
+  SECTION("Column of Location") {
+    int column_result = loc.Col();
+    REQUIRE(column_result == 10);
   }
 }
