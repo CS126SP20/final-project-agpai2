49eb145a9a5336e73b74e887d4823827634167ea
aniruddhapai <agpai2@illinois.edu>
2020-04-26 03:55:18 -0500

    Wrote test cases to check for change in direction and location

--- tests/test.cc
+++ tests/test.cc
@@ -75,3 +75,87 @@ TEST_CASE("Location Values", "[location]") {
     REQUIRE(column_result == 10);
   }
 }
+
+TEST_CASE("Change in Directions", "[engine-constructor][direction]") {
+  int row = 5;
+  int column = 10;
+
+  Engine engine(row, column);
+
+  SECTION("Initial Direction") {
+    engine.PlayerStep();
+    REQUIRE(engine.GetDirection() == Direction::kRight);
+  }
+
+  SECTION("Up Direction") {
+    engine.SetDirection(Direction::kUp);
+
+    REQUIRE(engine.GetDirection() == Direction::kUp);
+  }
+
+  SECTION("Down Direction") {
+    engine.SetDirection(Direction::kDown);
+    REQUIRE(engine.GetDirection() == Direction::kDown);
+  }
+
+  SECTION("Left Direction") {
+    engine.SetDirection(Direction::kLeft);
+    REQUIRE(engine.GetDirection() == Direction::kLeft);
+  }
+
+  SECTION("Right Direction") {
+    engine.SetDirection(Direction::kRight);
+    REQUIRE(engine.GetDirection() == Direction::kRight);
+  }
+
+  SECTION("Null Direction") {
+    engine.SetDirection(Direction::kNull);
+    REQUIRE(engine.GetDirection() == Direction::kNull);
+  }
+}
+
+TEST_CASE("Change in Location", "[direction][location]") {
+  int row = 0;
+  int column = 0;
+
+  Engine engine(row, column);
+
+  SECTION("Move Up") {
+    REQUIRE(engine.FromDirection(Direction::kUp) == Location{0, -1});
+  }
+
+  SECTION("Move Down") {
+    REQUIRE(engine.FromDirection(Direction::kDown) == Location{0, +1});
+  }
+
+  SECTION("Move Left") {
+    REQUIRE(engine.FromDirection(Direction::kLeft) == Location{-1, 0});
+  }
+
+  SECTION("Move Right") {
+    REQUIRE(engine.FromDirection(Direction::kRight) == Location{+1, 0});
+  }
+}
+
+TEST_CASE("Player's and Monster's Location", "[player][monster]") {
+  Player player(Location(2, 5));
+  Monster monster(Location(7,2));
+
+  SECTION("Player Location") {
+    REQUIRE(player.GetLoc() == Location{2,5});
+  }
+
+  SECTION("Player Location after a movement") {
+    player.SetLoc(Location(1,2));
+    REQUIRE(player.GetLoc() == Location{1,2});
+  }
+
+  SECTION("Monster Location") {
+    REQUIRE(monster.GetLoc() == Location{7,2});
+  }
+
+  SECTION("Monster Location after movement") {
+    monster.SetLoc(Location(6,9));
+    REQUIRE(monster.GetLoc() == Location{6,9});
+  }
+}
